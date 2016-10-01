090d6d5206cbca0f27ad04ab12f53e451ca5e2d1
Mykhailo Danylov <mike.danylov@gmail.com>
2016-10-01 19:56:09 +0300

    Add MONGOLAB_URI for production

--- db.js
+++ db.js
@@ -2,7 +2,13 @@
  * Created by mikedanylov on 10/1/16.
  */
 
-var uri = 'mongodb://localhost:27017/heatmaps-server';
+var uri;
+if (app.get('env') === 'development') {
+    uri = 'mongodb://localhost:27017/heatmaps-server';
+} else {
+    uri = process.ENV.MONGOLAB_URI;
+}
+
 var mongoose = require('mongoose');
 
 mongoose.connect(uri);
